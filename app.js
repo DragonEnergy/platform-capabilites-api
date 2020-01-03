252abe1fcfd8928a0e90e59e2dac1aee572fc303
Andy D <Andy@LP-PDX-ECO-LT09.lp.local>
2020-01-03 10:12:38 -0800

    heroku config

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const app = require('express')();
 const routes = require('./routes');
 
+var port = process.env.PORT || 8080;
+
 //  Connect all our routes to our application
 app.use('/api', routes);
 
 // Turn on that server!
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
+app.listen(port, () => {
+  console.log('App running on http://localhost:' + port);
 });
